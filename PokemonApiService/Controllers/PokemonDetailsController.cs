8102fa017829ca76a0a463e48b25e80e0b4fe023
ching yuan <52123873+chiyanYang@users.noreply.github.com>
2020-03-22 18:07:03 +0000

    init the project and add pokemon api package

---
+++ PokemonApiService/Controllers/PokemonDetailsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PokeAPI;
+
+namespace PokemonApiService.Controllers
+{
+    public class PokemonDetailsController : ControllerBase
+    {
+        private readonly ILogger<PokemonDetailsController> _logger;
+
+        public PokemonDetailsController(ILogger<PokemonDetailsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("pokemon/{pokemonName}")]
+        [HttpGet]
+        public async Task<string> Get(string pokemonName)
+        {
+            return pokemonName;
+        }
+    }
+}
---
+++ PokemonApiService/Modles/PokemonDetails.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PokemonApiService
+{
+    public class PokemonDetails
+    {
+        public string name { get; set; }
+
+        public string description { get; set; }
+
+    }
+}
