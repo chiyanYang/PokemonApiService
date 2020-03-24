9e2cd7069385227736bf5ae78e22868df4c48269
ching yuan <52123873+chiyanYang@users.noreply.github.com>
2020-03-24 08:23:00 +0000

    Added unit test for to test error handling

--- LiveTest/UnitTest.cs
+++ LiveTest/UnitTest.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using PokeAPI;
@@ -26,6 +27,7 @@ namespace LiveTest
         {
             const string content = "You gave Mr. Tim a hearty meal, but unfortunately what he ate made him die.";
             string translatedContent = await ShakespeareClient.getShakespeareTranslated(content);
+
             Assert.IsNotNull(translatedContent);
             Assert.AreEqual(translatedContent, "Thee did giveth mr. Tim a hearty meal,  but unfortunately what he did doth englut did maketh him kicketh the bucket.");
         }
@@ -34,12 +36,46 @@ namespace LiveTest
         public async Task TestPokemonDetailsAPI()
         {
             var controller = new PokemonDetailsController();
-            string response = await controller.Get(pokemonName);
-            PokemonDetails pokemonDetails = JsonConvert.DeserializeObject<PokemonDetails>(response);
+            var response = await controller.Get(pokemonName);
 
-            Assert.IsNotNull(pokemonDetails);
-            Assert.IsNotNull(pokemonDetails.Description);
-            Assert.AreEqual(pokemonDetails.Name, pokemonName);
+            Assert.IsNotNull(response);
+
+            if (response.StatusCode == StatusCodes.Status429TooManyRequests)
+            {
+                Assert.AreEqual(response.Value, "Exceed shakespeare API Ratelimit, Please try after a few minutes");
+            }
+            else
+            {
+                PokemonDetails pokemonDetails = response.Value as PokemonDetails;
+                Assert.IsNotNull(pokemonDetails);
+                Assert.IsNotNull(pokemonDetails.Description);
+                Assert.AreEqual(pokemonDetails.Name, pokemonName);
+            }
+        }
+
+        [TestMethod]
+        public async Task TestPokemonDetailsAPIWithNotAnPokemonName()
+        {
+            var controller = new PokemonDetailsController();
+            var response = await controller.Get("a3f");
+
+            Assert.IsNotNull(response);
+            Assert.AreEqual(response.StatusCode, StatusCodes.Status404NotFound);
+        }
+
+        [TestMethod]
+        public async Task TestShakespeareApiWhenReachingRateLimiter()
+        {
+            int count = 5;
+            const string content = "You gave Mr. Tim a hearty meal, but unfortunately what he ate made him die.";
+
+            while (count != 0)
+            {
+                await ShakespeareClient.getShakespeareTranslated(content);
+                count--;
+            }
+            string translatedContent = await ShakespeareClient.getShakespeareTranslated(content);
+            Assert.AreEqual(translatedContent, string.Empty);
         }
     }
 }
